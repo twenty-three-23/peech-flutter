c0db2d106cad4cf627d46f9c11a9a5f389b7a85f
Seungman Lim <woori4158@gmail.com>
2024-06-21 18:04:59 +0900

    refactor: 녹음-실행 코드를 view, controller로 분리
    - 이전 단계에서 추가했던 녹음-실행 테스트 코드를 GetX를 적용하여 view, controller 형식으로 리팩토링
    
    #TT-88 scriptInput

---
+++ lib/features/voiceRecode/controller/voice_recode_controller.dart
@@ -0,0 +1,69 @@
+import 'package:get/get.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+
+class VoiceRecodeCtr extends GetxController {
+
+  FlutterSoundRecorder? _recorder;
+  FlutterSoundPlayer? _player;
+  Rx<bool> isRecording = false.obs;
+  Rx<bool> isPlaying = false.obs;
+  final String _path = 'audio_recording.aac';
+
+  @override
+  void onInit() {
+    _recorder = FlutterSoundRecorder();
+    _player = FlutterSoundPlayer();
+    _openAudioSession();
+    super.onInit();
+  }
+
+
+  Future<void> _openAudioSession() async {
+    var status = await Permission.microphone.request();
+    if (status != PermissionStatus.granted) {
+      throw RecordingPermissionException('Microphone permission not granted');
+    }
+    await _recorder!.openRecorder();
+    await _player!.openPlayer();
+  }
+
+  @override
+  void onClose() {
+    _recorder!.closeRecorder();
+    _player!.closePlayer();
+    _recorder = null;
+    _player = null;
+    super.onClose();
+  }
+
+  Future<void> startRecording() async {
+    isRecording.value = true;
+    await _recorder!.startRecorder(
+      toFile: _path,
+      codec: Codec.aacADTS,
+    );
+  }
+
+  Future<void> stopRecording() async {
+    isRecording.value = false;
+    await _recorder!.stopRecorder();
+  }
+
+  Future<void> startPlaying() async {
+    isPlaying.value = true;
+    await _player!.startPlayer(
+      fromURI: _path,
+      codec: Codec.aacADTS,
+      whenFinished: () {
+        isPlaying.value = false;
+      },
+    );
+  }
+
+  Future<void> stopPlaying() async {
+    isPlaying.value = false;
+    await _player!.stopPlayer();
+  }
+
+}
--- lib/features/voiceRecode/view/voice_recode_screen.dart
+++ lib/features/voiceRecode/view/voice_recode_screen.dart
@@ -1,107 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:flutter_sound/flutter_sound.dart';
-import 'package:flutter_sound/public/flutter_sound_recorder.dart';
-import 'package:flutter_sound/public/flutter_sound_player.dart';
+import 'package:get/get.dart';
+import 'package:swm_peech_flutter/features/voiceRecode/controller/voice_recode_controller.dart';
 
 
-class VoiceRecodeScreen extends StatefulWidget {
-  @override
-  _VoiceRecodeScreenState createState() => _VoiceRecodeScreenState();
-}
-
-class _VoiceRecodeScreenState extends State<VoiceRecodeScreen> {
-  FlutterSoundRecorder? _recorder;
-  FlutterSoundPlayer? _player;
-  bool _isRecording = false;
-  bool _isPlaying = false;
-  String _path = 'audio_recording.aac';
-
-  @override
-  void initState() {
-    super.initState();
-    _recorder = FlutterSoundRecorder();
-    _player = FlutterSoundPlayer();
-    _openAudioSession();
-  }
-
-  Future<void> _openAudioSession() async {
-    var status = await Permission.microphone.request();
-    if (status != PermissionStatus.granted) {
-      throw RecordingPermissionException('Microphone permission not granted');
-    }
-    await _recorder!.openRecorder();
-    await _player!.openPlayer();
-  }
-
-  @override
-  void dispose() {
-    _recorder!.closeRecorder();
-    _player!.closePlayer();
-    _recorder = null;
-    _player = null;
-    super.dispose();
-  }
-
-  Future<void> _startRecording() async {
-    await _recorder!.startRecorder(
-      toFile: _path,
-      codec: Codec.aacADTS,
-    );
-    setState(() {
-      _isRecording = true;
-    });
-  }
-
-  Future<void> _stopRecording() async {
-    await _recorder!.stopRecorder();
-    setState(() {
-      _isRecording = false;
-    });
-  }
-
-  Future<void> _startPlaying() async {
-    await _player!.startPlayer(
-      fromURI: _path,
-      codec: Codec.aacADTS,
-      whenFinished: () {
-        setState(() {
-          _isPlaying = false;
-        });
-      },
-    );
-    setState(() {
-      _isPlaying = true;
-    });
-  }
-
-  Future<void> _stopPlaying() async {
-    await _player!.stopPlayer();
-    setState(() {
-      _isPlaying = false;
-    });
-  }
+class VoiceRecodeScreen extends StatelessWidget {
+  const VoiceRecodeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    final VoiceRecodeCtr _controller = Get.put(VoiceRecodeCtr());
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Audio Recorder'),
+        title: const Text('음성 녹음'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: _isRecording ? _stopRecording : _startRecording,
-              child: Text(_isRecording ? 'Stop Recording' : 'Start Recording'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _isPlaying ? _stopPlaying : _startPlaying,
-              child: Text(_isPlaying ? 'Stop Playing' : 'Start Playing'),
-            ),
-          ],
+      body: GetX<VoiceRecodeCtr>(
+        builder: (_) => Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: _controller.isRecording.value ? _controller.stopRecording : _controller.startRecording,
+                child: Text(_controller.isRecording.value ? 'Stop Recording' : 'Start Recording'),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _controller.isPlaying.value ? _controller.stopPlaying : _controller.startPlaying,
+                child: Text(_controller.isPlaying.value ? 'Stop Playing' : 'Start Playing'),
+              ),
+            ],
+          ),
         ),
       ),
     );
