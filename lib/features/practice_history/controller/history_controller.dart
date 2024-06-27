a21ba11a3c4e27d3591097de9bcb09b1876f735f
Seungman Lim <woori4158@gmail.com>
2024-06-27 15:24:44 +0900

    feat: TT-150 Major 목록 ui 구현
    - majorListView 구현
    - 리스너를 이용하여 경로가 바뀔 때 컨트롤러에서 해당하는 데이터를 불러오도록 설정
    - 더미데이터를 긴 한글 텍스트로 변경
    
    #TT-102 practiceHistory

--- lib/features/practice_history/controller/history_controller.dart
+++ lib/features/practice_history/controller/history_controller.dart
@@ -1,13 +1,17 @@
 import 'package:get/get.dart';
+import 'package:swm_peech_flutter/features/practice_history/data_source/history_major_data_source.dart';
 import 'package:swm_peech_flutter/features/practice_history/data_source/history_theme_data_source.dart';
+import 'package:swm_peech_flutter/features/practice_history/model/history_major_model.dart';
 import 'package:swm_peech_flutter/features/practice_history/model/history_path_model.dart';
 import 'package:swm_peech_flutter/features/practice_history/model/history_theme_model.dart';
 
 class HistoryCtr extends GetxController {
 
   Rx<List<HistoryThemeModel>?> themeList = Rx<List<HistoryThemeModel>?>(null);
+  Rx<List<HistoryMajorModel>?> majorList = Rx<List<HistoryMajorModel>?>(null);
 
   final historyThemeDataSource = HistoryThemeDataSource();
+  final historyMajorDataSource = HistoryMajorDataSource();
 
 
 
@@ -17,9 +21,31 @@ class HistoryCtr extends GetxController {
     themeList.value = await historyThemeDataSource.getThemeListTest();
   }
 
+  void getMajorList() async {
+    majorList.value = await historyMajorDataSource.getMajorListTest();
+  }
+
+  void addGetCurrentListListener() {
+    historyPath.value.pathState.listen((newState) {
+      switch(newState) {
+        case HistoryPathState.themeList:
+          getThemeList();
+          break;
+        case HistoryPathState.majorList:
+          getMajorList();
+          break;
+        case HistoryPathState.minorList:
+          break;
+        case HistoryPathState.minorDetail:
+          break;
+      }
+    });
+  }
+
   @override
   void onInit() {
     getThemeList();
+    addGetCurrentListListener();
     super.onInit();
   }
 
