f4af7db7ac7f3a0d3e6031a098ae70e1b76806b1
Seungman Lim <woori4158@gmail.com>
2024-07-06 19:10:32 +0900

    feat: TT-143 테마 [입력 완료] 버튼에 api연동
    - 테마 입력의 [입력 완료] 버튼 클릭시 post요청으로 테마를 보내고 테마 id를 받아옴. 받아온 테마id는 Local에 저장함.
    
    #TT-143 themePostApi

--- lib/features/theme_input/controller/theme_input_controller.dart
+++ lib/features/theme_input/controller/theme_input_controller.dart
@@ -1,6 +1,13 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:swm_peech_flutter/features/common/data_source/local/local_device_uuid_storage.dart';
 import 'package:swm_peech_flutter/features/common/data_source/local/local_practice_theme_storage.dart';
+import 'package:swm_peech_flutter/features/common/data_source/local/local_user_token_storage.dart';
+import 'package:swm_peech_flutter/features/common/dio_intercepter/auth_token_inject_interceptor.dart';
+import 'package:swm_peech_flutter/features/common/dio_intercepter/auth_token_refresh_intercepter.dart';
+import 'package:swm_peech_flutter/features/common/models/theme_id_model.dart';
+import 'package:swm_peech_flutter/features/theme_input/data_source/remote/remote_theme_save_data_source.dart';
 import '../../common/data_source/local/local_practice_mode_storage.dart';
 
 class ThemeInputCtr extends GetxController {
@@ -11,7 +18,28 @@ class ThemeInputCtr extends GetxController {
   }
 
   Future<void> saveTheme() async {
+    if(_theme == null || _theme == "") {
+      throw Exception("error: theme is null or blank");
+    }
     await LocalPracticeThemeStorage().setThemeText(_theme);
+    final ThemeIdModel themeIdModel = await getThemeId(_theme ?? "");
+    await LocalPracticeThemeStorage().setThemeId(themeIdModel.themeId);
+  }
+
+  Future<ThemeIdModel> getThemeId(String theme) async {
+    try {
+      Dio dio = Dio();
+      //TODO 동시성 문제 발생하지 않는지 궁금
+      dio.interceptors.add(AuthTokenInjectInterceptor(localUserTokenStorage: LocalUserTokenStorage()));
+      dio.interceptors.add(AuthTokenRefreshInterceptor(localDeviceUuidStorage: LocalDeviceUuidStorage(), localUserTokenStorage: LocalUserTokenStorage()));
+      final remoteThemeSaveDataSource = RemoteThemeSaveDataSource(dio);
+      final ThemeIdModel themeIdModel = await remoteThemeSaveDataSource.postTheme({"themeTitle": theme});
+      print("테마id: ${themeIdModel.themeId}");
+      return themeIdModel;
+    } on DioException catch(e) {
+      print("status: ${e.response?.statusCode}, message: ${e.response?.data["message"]}");
+      throw(Exception("bad request"));
+    }
   }
 
   void finishButton(BuildContext context) async {
