a7ba68d08a6f5f68a9f514df47000f155d422be0
Seungman Lim <woori4158@gmail.com>
2024-07-06 13:07:40 +0900

    rename: dio intercepter 이름 변경 (DioTokenIntercepter -> AuthTokenInjectIntercepter)
    - intercepter의 역할을 더 명시적으로 표현하기 위해 이름 변경
    
    #TT-204 userIdAssignApi

--- lib/features/common/dio_intercepter/auth_token_inject_interceptor.dart
+++ lib/features/common/dio_intercepter/auth_token_inject_interceptor.dart
@@ -1,10 +1,10 @@
 import 'package:dio/dio.dart';
 import 'package:swm_peech_flutter/features/common/data_source/local/local_user_token_storage.dart';
 
-class DioTokenIntercepter extends Interceptor {
+class AuthTokenInjectInterceptor extends Interceptor {
   final LocalUserTokenStorage localUserTokenStorage;
 
-  DioTokenIntercepter({required this.localUserTokenStorage});
+  AuthTokenInjectInterceptor({required this.localUserTokenStorage});
 
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
