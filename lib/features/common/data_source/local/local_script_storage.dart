985244384abaaed343f7a278754ec76aed38b695
Seungman Lim <woori4158@gmail.com>
2024-07-10 20:02:04 +0900

    feat: TT-194 LocalScriptStorage에 scriptId저장 기능 구현
    - LocalScriptStorage에 script 내용만 저장할 수 있었는데, scriptId를 저장할 수 있도록 기능 확장
    
    #TT-194 feature scriptInputApi

--- lib/features/common/data_source/local/local_script_storage.dart
+++ lib/features/common/data_source/local/local_script_storage.dart
@@ -6,7 +6,8 @@ class LocalScriptStorage {
   static final LocalScriptStorage _instance = LocalScriptStorage._internal();
   late SharedPreferences _prefs;
 
-  static const CURRENT_SCRIPT_KEY = "currentScript";
+  static const CURRENT_SCRIPT_CONTENT_KEY = "currentScriptContent";
+  static const CURRENT_SCRIPT_ID_KEY = "currentScriptId";
 
   factory LocalScriptStorage() {
     return _instance;
@@ -19,14 +20,24 @@ class LocalScriptStorage {
   }
 
 
-  Future<void> setScript(List<String> scriptList) async {
+  Future<void> setScriptContent(List<String> scriptList) async {
     debugPrint("저장함: ${scriptList.length}줄");
-    await _prefs.setStringList(CURRENT_SCRIPT_KEY, scriptList);
+    await _prefs.setStringList(CURRENT_SCRIPT_CONTENT_KEY, scriptList);
   }
 
-  List<String>? getScript() {
-    debugPrint("불러옴: ${_prefs.getStringList(CURRENT_SCRIPT_KEY)?.length}줄");
-    return _prefs.getStringList(CURRENT_SCRIPT_KEY);
+  List<String>? getScriptContent() {
+    debugPrint("불러옴: ${_prefs.getStringList(CURRENT_SCRIPT_CONTENT_KEY)?.length}줄");
+    return _prefs.getStringList(CURRENT_SCRIPT_CONTENT_KEY);
+  }
+
+  Future<void> setScriptId(int scriptId) async {
+    debugPrint("저장함: scriptId: ${scriptId}");
+    await _prefs.setInt(CURRENT_SCRIPT_ID_KEY, scriptId);
+  }
+
+  int? getScriptId() {
+    debugPrint("불러옴: scriptId: ${_prefs.getInt(CURRENT_SCRIPT_ID_KEY)}");
+    return _prefs.getInt(CURRENT_SCRIPT_ID_KEY);
   }
 
 }
