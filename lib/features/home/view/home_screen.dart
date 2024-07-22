a96120cb278a5aa8172021fde1bcb1c6aafbb6c8
Seungman Lim <woori4158@gmail.com>
2024-07-22 16:06:43 +0900

    design: TT-295 홈 화면 디자인 수정
    - 서비스가 줄 수 있는 가치 명시적으로 표시
    - 사용 가능 시간 refresh icon 위치 수정 및 클릭 범위 증가
    - 버튼에 배경 추가
    
    #TT-295 #101

--- lib/features/home/view/home_screen.dart
+++ lib/features/home/view/home_screen.dart
@@ -18,76 +18,91 @@ class HomeScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text(
-              "Peech - 당신의 발표 도우미",
+              "Peech - 당신의 발표 연습 도우미",
+              style: TextStyle(
+                fontSize: 23,
+              ),
+            ),
+            const SizedBox(height: 30,),
+            const Text(
+              "1. 발표 속도 조절 \n2. 자동 대본 생성 \n3. 연습 기록 관리",
               style: TextStyle(
                 fontSize: 20,
               ),
             ),
             const SizedBox(height: 50,),
             GetX<HomeCtr>(
-              builder: (_) => Column(
+              builder: (_) => Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
+                  const SizedBox(width: 40,),
+                  Column(
                     children: [
-                      const Text("총"),
-                      const SizedBox(width: 3,),
-                      controller.remainingTime.value == null
-                          ? const SizedBox(width: 15, height: 15, child: CircularProgressIndicator(strokeWidth: 2,))
-                          : Text(
-                              controller.remainingTime.value?.text ?? "?",
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                              )
-                            ),
-                      const SizedBox(width: 3,),
-                      const Text("사용 가능"),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const Text("총"),
+                          const SizedBox(width: 3,),
+                          controller.remainingTime.value == null
+                              ? const SizedBox(width: 15, height: 15, child: CircularProgressIndicator(strokeWidth: 2,))
+                              : Text(
+                                  controller.remainingTime.value?.text ?? "?",
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  )
+                                ),
+                          const SizedBox(width: 3,),
+                          const Text("사용 가능"),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const Text("1회 최대"),
+                          const SizedBox(width: 3,),
+                          controller.maxAudioTime.value == null
+                              ? const SizedBox(width: 15, height: 15, child: CircularProgressIndicator(strokeWidth: 2,))
+                              : Text(
+                                  controller.maxAudioTime.value?.text ?? "?",
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  )
+                                ),
+                          const SizedBox(width: 3,),
+                          const Text("연습 가능"),
+                        ],
+                      ),
                     ],
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Text("1회 최대"),
-                      const SizedBox(width: 3,),
-                      controller.maxAudioTime.value == null
-                          ? const SizedBox(width: 15, height: 15, child: CircularProgressIndicator(strokeWidth: 2,))
-                          : Text(
-                              controller.maxAudioTime.value?.text ?? "?",
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                              )
-                            ),
-                      const SizedBox(width: 3,),
-                      const Text("연습 가능"),
-                    ],
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      controller.getUserAudioTimeInfo();
-                    },
-                    child: const Icon(Icons.refresh, size: 17,),
-                  ),
+                  SizedBox(
+                    width: 40,
+                    height: 40,
+                    child: IconButton(
+                        onPressed: () { controller.getUserAudioTimeInfo(); },
+                        icon: const Icon(Icons.refresh, size: 17,)
+                    ),
+                  )
                 ],
               ),
             ),
-            const SizedBox(height: 30,),
-            TextButton(
+            const SizedBox(height: 50,),
+            ElevatedButton(
                 onPressed: () {
                     LocalPracticeModeStorage().setMode(PracticeMode.withScript);
                     Navigator.pushNamed(context, '/themeInput');
                   },
                 child: const Text("대본 입력하고 시작하기")
             ),
-            TextButton(
+            ElevatedButton(
                 onPressed: () {
                     LocalPracticeModeStorage().setMode(PracticeMode.noScript);
                     Navigator.pushNamed(context, '/themeInput');
                   },
                 child: const Text("대본 입력하지 않고 시작하기")
             ),
-            TextButton(
+            ElevatedButton(
                 onPressed: () { Navigator.pushNamed(context, '/historyThemeList'); },
                 child: const Text("기록 보기")
             ),
