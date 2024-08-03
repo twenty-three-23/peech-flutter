546c2396365131af05c6a1e2fa95a90080536736
Seungman Lim <woori4158@gmail.com>
2024-08-03 15:07:22 +0900

    feat: TT-343 로그인 로직 수정
    - 디바이스 아이디 대신 RT로 refresh 하도록 수정 (서버 미완료로 일부 로직 작성 못함)
    - 로그인 로직 수정으로 필요없어진 interceptor 제거
    
    #feature/TT-343-kakaoLogin #136

--- lib/features/common/dio/auth_dio_factory.dart
+++ lib/features/common/dio/auth_dio_factory.dart
@@ -1,8 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:swm_peech_flutter/features/common/data_source/local/local_auth_token_storage.dart';
-import 'package:swm_peech_flutter/features/common/data_source/local/local_device_uuid_storage.dart';
 import 'package:swm_peech_flutter/features/common/dio_intercepter/auth_token_inject_interceptor.dart';
-import 'package:swm_peech_flutter/features/common/dio_intercepter/auto_register_interceptor.dart';
+import 'package:swm_peech_flutter/features/common/dio_intercepter/auth_token_refresh_interceptor.dart';
 import 'package:swm_peech_flutter/features/common/dio_intercepter/debug_interceptor.dart';
 
 class AuthDioFactory {
@@ -11,7 +10,7 @@ class AuthDioFactory {
 
     dio.interceptors.addAll([
       AuthTokenInjectInterceptor(localAuthTokenStorage: LocalAuthTokenStorage()),
-      AutoTokenRegisterIntercepter(localDeviceUuidStorage: LocalDeviceUuidStorage()),
+      AuthTokenRefreshInterceptor(localAuthTokenStorage: LocalAuthTokenStorage()),
       DebugIntercepter(),
     ]);
 
