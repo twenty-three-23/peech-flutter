26b0d82d577430f151c5b639be4968258d2a16bd
Seungman Lim <woori4158@gmail.com>
2024-08-16 21:45:52 +0900

    design: TT-363 대본 입력 화면 디자인 추가
    #feature/TT-363-improveUiUx #159

--- lib/features/common/widgets/common_text_field.dart
+++ lib/features/common/widgets/common_text_field.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 
 class CommonTextField extends StatefulWidget {
-  final int maxLines;
+  final int minLines;
+  final int? maxLines;
   final Function(String) onChanged;
   final String hintText;
+  final bool showCounter;
+  final TextEditingController? controller;
 
-  const CommonTextField({super.key, this.maxLines = 1, this.onChanged = _defaultOnChanged, this.hintText = ''});
+  const CommonTextField({
+    super.key,
+    this.minLines = 1,
+    this.maxLines,
+    this.onChanged = _defaultOnChanged,
+    this.hintText = '',
+    this.showCounter = false,
+    this.controller
+  });
 
   static void _defaultOnChanged(String value) {
     // 아무 동작도 하지 않는 기본 함수
@@ -18,31 +29,35 @@ class CommonTextField extends StatefulWidget {
 class _CommonTextFieldState extends State<CommonTextField> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 48,
-      width: 312,
-      child: TextField(
-        maxLines: 1,
-        onChanged: (value) => widget.onChanged(value),
-        style: const TextStyle(
-          fontSize: 16,
-        ),
-        decoration: InputDecoration(
-          hintText: widget.hintText,
-          contentPadding: const EdgeInsets.symmetric(horizontal: 12),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16),
-            borderSide: const BorderSide(
-                color: Colors.black,
-                width: 1.2
-            ),
+    return TextField(
+      controller: widget.controller,
+      minLines: widget.minLines,
+      maxLines: widget.maxLines,
+      onChanged: (value) => widget.onChanged(value),
+      style: const TextStyle(
+        fontSize: 16,
+      ),
+      buildCounter: (context,
+          {required currentLength, required isFocused, maxLength}) {
+        if(widget.showCounter) {
+          return Text("$currentLength 자");
+        }
+      },
+      decoration: InputDecoration(
+        hintText: widget.hintText,
+        contentPadding: const EdgeInsets.all(12.0),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: const BorderSide(
+              color: Colors.black,
+              width: 1.2
           ),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16),
-            borderSide: const BorderSide(
-              color: Color(0xFFE5E8F0),
-              width: 1
-            ),
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: const BorderSide(
+            color: Color(0xFFE5E8F0),
+            width: 1
           ),
         ),
       ),
--- lib/features/script_input/view/script_input_screen.dart
+++ lib/features/script_input/view/script_input_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:swm_peech_flutter/features/common/widgets/common_text_field.dart';
+import 'package:swm_peech_flutter/features/common/widgets/primary_color_button.dart';
 import '../controller/script_input_controller.dart';
 
 class ScriptInputScreen extends StatefulWidget {
@@ -23,86 +26,136 @@ class _ScriptInputScreenState extends State<ScriptInputScreen> {
         leading: IconButton(onPressed: () { Navigator.of(context).pop(); }, icon: const Icon(Icons.arrow_back_ios)),
         title: const Text("대본으로 시작"),
       ),
-      body: GetX<ScriptInputCtr>(
-        builder: (_) => controller.scriptInputIsLoading.value == true
-        ? const Center(child: CircularProgressIndicator(),)
-        : SingleChildScrollView(
-            child: Column(
-              children: [
-                const SizedBox(height: 20,),
-                const Text("대본을 입력해주세요"),
-                const SizedBox(height: 20,),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Center(
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      primary: false,
-                      itemCount: controller.script.value.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Expanded(
-                                  child: TextField(
-                                    controller: controller.script.value[index],
-                                    minLines: 1,
-                                    maxLines: null,
-                                    onChanged: (value) {
-                                      controller.updateScript(index, value);
-                                    },
-                                    buildCounter: (context,
-                                        {required currentLength, required isFocused, maxLength}) {
-                                      return Text("$currentLength 자");
-                                    },
-                                    decoration: const InputDecoration(
-                                        hintText: "문단을 입력해주세요",
-                                        border: OutlineInputBorder(),
-                                    ),
-                                  ),
-                                ),
-                                Column(
-                                  children: [
-                                    Text(
-                                      "${index + 1}문단",
-                                      style: const TextStyle(fontSize: 12),
-                                    ),
-                                    IconButton(
-                                      onPressed: () { controller.removeParagraph(index); },
-                                      icon: const Icon(Icons.close)
-                                    ),
-                                  ],
-                                )
-                              ],
-                            ),
-                            const SizedBox(height: 15,),
-                          ],
-                        );
-                      },
-                    )
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus(); // 화면을 터치하면 키보드를 내리고 포커스를 해제합니다.
+        },
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                children: [
+                  SvgPicture.asset(
+                      'assets/images/progress_indicator_filled.svg',
+                      semanticsLabel: 'progress bar'
                   ),
+                  const SizedBox(width: 4,),
+                  SvgPicture.asset(
+                      'assets/images/progress_indicator_filled.svg',
+                      semanticsLabel: 'progress bar'
+                  ),
+                  const SizedBox(width: 4,),
+                  SvgPicture.asset(
+                      'assets/images/progress_indicator_empty.svg',
+                      semanticsLabel: 'progress bar'
+                  ),
+                  const SizedBox(width: 4,),
+                  SvgPicture.asset(
+                      'assets/images/progress_indicator_empty.svg',
+                      semanticsLabel: 'progress bar'
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: GetX<ScriptInputCtr>(
+                builder: (_) => controller.scriptInputIsLoading.value == true
+                ? const Center(child: CircularProgressIndicator(),)
+                : SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 16,),
+                          const Text(
+                              "발표할 내용을 적어보세요",
+                              style: TextStyle(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w400,
+                                  color: Color(0xFF3B3E43)
+                              )
+                          ),
+                          const SizedBox(height: 8,),
+                          const Text(
+                              "발표할 내용을 적으면 예상 시간을 알 수 있어요",
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                  color: Color(0xFF3B3E43)
+                              )
+                          ),
+                          const SizedBox(height: 24,),
+                          ListView.builder(
+                            shrinkWrap: true,
+                            primary: false,
+                            itemCount: controller.script.value.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Expanded(
+                                        child: CommonTextField(
+                                          controller: controller.script.value[index],
+                                          hintText: '문단을 입력하세요',
+                                          minLines: 1,
+                                          maxLines: 10,
+                                          onChanged: (value) {
+                                            controller.updateScript(index, value);
+                                          },
+                                          showCounter: true,
+                                        )
+                                      ),
+                                      Column(
+                                        children: [
+                                          Text(
+                                            "${index + 1}문단",
+                                            style: const TextStyle(fontSize: 12),
+                                          ),
+                                          IconButton(
+                                            onPressed: () { controller.removeParagraph(index); },
+                                            icon: const Icon(Icons.close)
+                                          ),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                  const SizedBox(height: 15,),
+                                ],
+                              );
+                            },
+                          ),
+                          TextButton(
+                            onPressed: () { controller.addParagraph(); },
+                            child: const Text("문단 추가")
+                          ),
+                        ],
+                      ),
+                    ),
                 ),
-                TextButton(
-                  onPressed: () { controller.addParagraph(); },
-                  child: const Text("문단 추가")
-                ),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
-                        child: const Text("대본 입력 완료"),
+              ),
+            ),
+            if(controller.scriptInputIsLoading.value == false)
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: PrimaryColorButton(
+                        text: '대본 입력 완료',
                         onPressed: () {
                           controller.inputConfirmBtn(context);
                         },
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            const SizedBox(height: 16,)
+          ],
         ),
       ),
     );
