e6df9801cc65706ce1e9956f541069c9901b0669
Seungman Lim <woori4158@gmail.com>
2024-08-17 16:45:44 +0900

    design: TT-363 주제로 시작하기 버튼 디자인 추가
    #feature/TT-363-improveUiUx #159

--- lib/features/practice_history/widgets/history_list_view.dart
+++ lib/features/practice_history/widgets/history_list_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:swm_peech_flutter/features/common/data_source/local/local_practice_mode_storage.dart';
+import 'package:swm_peech_flutter/features/common/widgets/primary_color_button.dart';
 import 'package:swm_peech_flutter/features/practice_history/controller/history_controller.dart';
 import 'package:swm_peech_flutter/features/practice_history/model/history_path_model.dart';
 import 'package:swm_peech_flutter/features/practice_history/widgets/major_list_view.dart';
@@ -13,15 +15,40 @@ Widget historyListView(BuildContext context, HistoryCtr controller) {
     case HistoryPathState.majorList:
       return Column(
         children: [
-          TextButton(
-              onPressed: () { controller.startWithThemeWithScriptBtn(context); },
-              child: const Text("이 주제로 시작하기(대본 입력 O)")
-          ),
-          TextButton(
-              onPressed: () { controller.startWithThemeNoScriptBtn(context); },
-              child: const Text("이 주제로 시작하기(대본 입력 X)")
-          ),
           Expanded(child: majorListView(controller)),
+          Row(
+            children: [
+              Expanded(
+                child: ColoredButton(
+                  text: '이 주제로 시작하기',
+                  onPressed: () {
+                    controller.startWithThemeWithScriptBtn(context);
+                  },
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 8,),
+          Row(
+            children: [
+              Expanded(
+                child: TextButton(
+                    onPressed: () {
+                      controller.startWithThemeNoScriptBtn(context);
+                    },
+                    child: const Text(
+                        "이 주제로 대본없이 녹음 바로 하기",
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xFF3B3E43),
+                          height: 22 / 14,
+                        )
+                    )
+                ),
+              ),
+            ],
+          ),
         ],
       );
     case HistoryPathState.minorList:
