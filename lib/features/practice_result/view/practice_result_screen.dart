01b2f36e427bf81314501203fc3ac9af2e5b9ffc
Seungman Lim <woori4158@gmail.com>
2024-08-17 20:04:23 +0900

    design: TT-363 분석 결과 화면 디자인 추가
    #feature/TT-363-improveUiUx #159

--- lib/features/practice_result/view/practice_result_screen.dart
+++ lib/features/practice_result/view/practice_result_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:swm_peech_flutter/features/common/widgets/primary_color_button.dart';
 import 'package:swm_peech_flutter/features/practice_result/controller/practice_result_controller.dart';
 import 'package:swm_peech_flutter/features/practice_result/widget/editing_dialog.dart';
 
@@ -21,21 +22,56 @@ class PracticeResultScreen extends StatelessWidget {
         title: const Text("연습 결과"),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
         child: GetX<PracticeResultCtr>(
             builder: (_) => Column(
               children: [
                 if(controller.isLoading.value == false)
-                  Text(
-                    "전체 시간: ${controller.practiceResult.value?.totalTime == null
-                    ? controller.practiceResult.value?.totalRealTime ?? "??:??:??"
-                    : controller.practiceResult.value?.totalTime ?? "??:??:??"}",
-                    style: const TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold
-                    ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 16,),
+                      const Text(
+                          "분석 결과에요",
+                          style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w700,
+                              color: Color(0xFF3B3E43),
+                              height: 34 / 22
+                          )
+                      ),
+
+                      const SizedBox(height: 8,),
+                      Row(
+                        children: [
+                          const Icon(Icons.timer_outlined),
+                          const SizedBox(width: 4,),
+                          Text(
+                              '총 ${controller.practiceResult.value?.totalTime == null
+                                  ? controller.practiceResult.value?.totalRealTime ?? "??:??:??"
+                                  : controller.practiceResult.value?.totalTime ?? "??:??:??"}',
+                              style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w700,
+                                  color: Color(0xFF3B3E43),
+                                  height: 26 / 18
+                              )
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 8,),
+                      const Text(
+                          "원하는 대로 대본을 수정하고, 예상 시간을 받아볼 수 있어요",
+                          style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              color: Color(0xFF3B3E43),
+                              height: 22 / 14
+                          )
+                      ),
+                    ],
                   ),
-                const SizedBox(height: 10),
+                const SizedBox(height: 24),
                 Expanded(
                   child: controller.isLoading.value == true
                       ? const Column(
@@ -238,53 +274,81 @@ class PracticeResultScreen extends StatelessWidget {
                           },
                         ),
                 ),
-                const Divider(height: 1,),
+                const SizedBox(height: 8,),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     if(controller.isLoading.value == false)
-                      ElevatedButton(
-                          onPressed: () { controller.editingFinishBtn();  },
-                          child: const Text("수정사항 반영하기")
+                      Expanded(
+                        child: ColoredButton(
+                          text: '수정사항 적용',
+                          onPressed: () { controller.editingFinishBtn(); },
+                          backgroundColor: const Color(0xFF3B3E43),
+                          textColor: const Color(0xFFFFFFFF),
+                        ),
                       ),
-                    ElevatedButton(
-                      onPressed: () {
-                        showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return AlertDialog(
-                                title: const Text("홈으로 나가기"),
-                                content: const IntrinsicHeight(
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text("반영되지 않은 수정사항은 저장되지 않습니다. 수정 사항이 있다면 [수정사항 반영하기] 버튼을 눌러주세요"),
-                                      SizedBox(height: 10,),
-                                      Text("연습한 기록은 홈 화면의 [기록 보기] 에서 확인하실 수 있습니다."),
-                                      SizedBox(height: 10,),
-                                      Text("홈으로 나가시겠습니까?")
-                                    ],
+                    const SizedBox(width: 8,),
+                    Expanded(
+                      child: ColoredButton(
+                        backgroundColor: const Color(0xFF3B3E43),
+                        textColor: const Color(0xFFFFFFFF),
+                        text: '나가기',
+                        onPressed: () {
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: const Text(
+                                    "나가기",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w700,
+                                      height: 26 / 18,
+                                    )
                                   ),
-                                ),
-                                actions: [
-                                  TextButton(
-                                    onPressed: () { Navigator.of(context).pop(); },
-                                    child: const Text("취소"),
+                                  content: const Text(
+                                    "반영되지 않은 수정 사항이 있다면\n저장하기를 눌러주세요\n'기록보기'에서 확인할 수 있습니다.",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w400,
+                                      height: 24 / 16,
+                                    ),
                                   ),
-                                  TextButton(
-                                    onPressed: () { controller.homeBtn(context); },
-                                    child: const Text("홈으로 나가기"),
-                                  )
-                                ],
-
-                              );
-                            }
-                        );
-                      },
-                      child: const Text("홈")
+                                  actions: [
+                                    Row(
+                                      children: [
+                                        Expanded(
+                                          child: ColoredButton(
+                                            text: '닫기',
+                                            onPressed: () { Navigator.of(context).pop(); },
+                                            backgroundColor: const Color(0xFFF4F6FA),
+                                            textColor: const Color(0xFF3B3E43),
+                                          ),
+                                        ),
+                                        const SizedBox(width: 8,),
+                                        Expanded(
+                                          child: ColoredButton(
+                                            text: '나가기',
+                                            onPressed: () { controller.homeBtn(context); },
+                                            backgroundColor: const Color(0xFF3B3E43),
+                                            textColor: const Color(0xFFFFFFFF),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                      
+                                );
+                              }
+                          );
+                        },
+                      ),
                     ),
                   ],
-                )
+                ),
+                const SizedBox(height: 8,),
               ],
             )
         ),
