50bb042609497f83a03192f0b90f2f283314326a
Seungman Lim <woori4158@gmail.com>
2024-08-30 14:28:55 +0900

    remove: TT-395 필요없는 코드 제거
    - 더이상 필요없는 device uuid 관련 코드를 제거함
    
    #feature/TT-395-deployWeb #184

--- lib/initializer/local_storage_initializer.dart
+++ lib/initializer/local_storage_initializer.dart
@@ -1,21 +1,15 @@
 import 'package:swm_peech_flutter/features/common/data_source/local/local_auth_token_storage.dart';
-import 'package:swm_peech_flutter/features/common/data_source/local/local_device_uuid_storage.dart';
 import 'package:swm_peech_flutter/features/common/data_source/local/local_practice_mode_storage.dart';
 import 'package:swm_peech_flutter/features/common/data_source/local/local_practice_theme_storage.dart';
 import 'package:swm_peech_flutter/features/common/data_source/local/local_script_storage.dart';
 
 class LocalStorageInitializer {
-
   Future<void> initialize() async {
-
     await Future.wait([
       LocalScriptStorage().init(),
       LocalPracticeModeStorage().init(),
       LocalPracticeThemeStorage().init(),
-      LocalDeviceUuidStorage().init(),
       LocalAuthTokenStorage().init(),
     ]);
-
   }
-
 }
