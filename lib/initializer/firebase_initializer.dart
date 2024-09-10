afb4e3786aa4badfc51df75766c1b89474ee773c
Seungman Lim <woori4158@gmail.com>
2024-09-10 15:59:23 +0900

    feat: TT-418 web GA를 위한 설정
    - 웹 GA는 작동하지 않는 것으로 확인
    
    #feature/TT-418-GoogleAnayltics #200

--- lib/initializer/firebase_initializer.dart
+++ lib/initializer/firebase_initializer.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:swm_peech_flutter/firebase_options.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics_web/firebase_analytics_web.dart';
 
 class FirebaseInitializer {
   Future<void> initialize() async {
