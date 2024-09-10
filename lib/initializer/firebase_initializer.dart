6525a87139ac12cf376e3ef6c44d789aa5dd3e05
Seungman Lim <woori4158@gmail.com>
2024-09-10 12:51:19 +0900

    feat: TT-418 android, ios GA달기
    - 안드로이드 및 IOS에 Google Analytics를 적용함
    
    #feature/TT-418-GoogleAnayltics #200

---
+++ lib/initializer/firebase_initializer.dart
@@ -0,0 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:swm_peech_flutter/firebase_options.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+
+class FirebaseInitializer {
+  Future<void> initialize() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  }
+}
