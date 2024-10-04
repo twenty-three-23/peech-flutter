e8a02526c2ce5920708db7b8c33e36037babef15
Seungman Lim <woori4158@gmail.com>
2024-10-04 17:07:15 +0900

    feat: TT-455 바텀 네비게이션에 화면 연결
    - RootScreen에 바텀 네비게이션을 추가하고 RootScreen을 초기 화면으로 설정
    - 바텀 네비게이션에 HomeScreen(), HistoryScreen() 추가
    - CommonScaffold에 app bar 숨길 수 있는 옵션 구현
    
    #feature/TT-455-bottomNavigation #236

--- lib/features/common/widgets/common_scaffold.dart
+++ lib/features/common/widgets/common_scaffold.dart
@@ -7,6 +7,7 @@ class CommonScaffold extends StatefulWidget {
   final Widget child;
   final Function()? backAction;
   final Widget? bottomNavigationBar;
+  final bool hideAppBar;
 
   CommonScaffold({
     super.key,
@@ -14,6 +15,7 @@ class CommonScaffold extends StatefulWidget {
     required this.child,
     this.backAction,
     this.bottomNavigationBar,
+    this.hideAppBar = false,
   });
 
   @override
@@ -26,46 +28,48 @@ class _CommonScaffoldState extends State<CommonScaffold> {
     return Scaffold(
       backgroundColor: Colors.white,
       bottomNavigationBar: widget.bottomNavigationBar,
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: Colors.white,
-        title: SizedBox(
-          height: 56,
-          child: Stack(
-            children: [
-              if (widget.appBarTitle != null)
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Row(
-                    children: [
-                      IconButton(
-                          onPressed: () {
-                            if (widget.backAction == null) {
-                              Navigator.of(context).pop();
-                            } else {
-                              widget.backAction!();
-                            }
-                          },
-                          icon: const Icon(Icons.arrow_back_ios)),
-                      Text(widget.appBarTitle ?? '',
-                          style: const TextStyle(fontSize: 22, fontWeight: FontWeight.w700, color: Color(0xFF3B3E43), height: 34 / 22)),
-                    ],
-                  ),
-                )
-              else
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: SvgPicture.asset(
-                    'assets/images/app_bar_logo.svg',
-                    semanticsLabel: 'peech app bar logo',
-                    height: 25,
-                    fit: BoxFit.contain,
-                  ),
+      appBar: widget.hideAppBar == true
+          ? null
+          : AppBar(
+              automaticallyImplyLeading: false,
+              backgroundColor: Colors.white,
+              title: SizedBox(
+                height: 56,
+                child: Stack(
+                  children: [
+                    if (widget.appBarTitle != null)
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Row(
+                          children: [
+                            IconButton(
+                                onPressed: () {
+                                  if (widget.backAction == null) {
+                                    Navigator.of(context).pop();
+                                  } else {
+                                    widget.backAction!();
+                                  }
+                                },
+                                icon: const Icon(Icons.arrow_back_ios)),
+                            Text(widget.appBarTitle ?? '',
+                                style: const TextStyle(fontSize: 22, fontWeight: FontWeight.w700, color: Color(0xFF3B3E43), height: 34 / 22)),
+                          ],
+                        ),
+                      )
+                    else
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: SvgPicture.asset(
+                          'assets/images/app_bar_logo.svg',
+                          semanticsLabel: 'peech app bar logo',
+                          height: 25,
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                  ],
                 ),
-            ],
-          ),
-        ),
-      ),
+              ),
+            ),
       body: SafeArea(child: widget.child),
     );
   }
---
+++ lib/features/root/view/root_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:swm_peech_flutter/features/common/widgets/common_scaffold.dart';
+import 'package:swm_peech_flutter/features/common/widgets/custom_bottom_navigation.dart';
+import 'package:swm_peech_flutter/features/home/controller/home_controller.dart';
+import 'package:swm_peech_flutter/features/home/view/home_screen.dart';
+import 'package:swm_peech_flutter/features/practice_history/controller/history_controller.dart';
+import 'package:swm_peech_flutter/features/practice_history/view/history_screen.dart';
+
+class RootScreen extends StatefulWidget {
+  RootScreen({super.key});
+
+  @override
+  State<RootScreen> createState() => _RootScreenState();
+}
+
+class _RootScreenState extends State<RootScreen> {
+  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    Get.lazyPut<HomeCtr>(() => HomeCtr());
+    Get.lazyPut<HistoryCtr>(() => HistoryCtr());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CommonScaffold(
+      hideAppBar: true,
+      bottomNavigationBar: CustomBottomNavigation(
+        initialIndex: 0,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+      child: IndexedStack(
+        index: _currentIndex,
+        children: [
+          HomeScreen(),
+          HistoryScreen(),
+        ],
+      ),
+    );
+  }
+}
