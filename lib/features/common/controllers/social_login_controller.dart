182f221ae297562e279271126403a4abdf68f38a
snacktime <tth8181@gmail.com>
2024-10-13 15:34:18 +0900

    fix: TT-469 이름 입력 삭제, 생년월일 선택. 개인정보 동의서
    애플 출시 기준에 맞추기 위해, 이름 입력을 삭제
    생년월일을 선택으로 바꾸고 기본으로 1900,0,0 으로 전달
    회원가입 전 개인정보 활용 동의

--- lib/features/common/controllers/social_login_controller.dart
+++ lib/features/common/controllers/social_login_controller.dart
@@ -23,7 +23,9 @@ import 'package:swm_peech_flutter/features/common/models/user_additional_info_mo
 import 'package:swm_peech_flutter/features/common/models/user_additional_info_view_state.dart';
 import 'package:swm_peech_flutter/features/common/models/user_gender.dart';
 import 'package:swm_peech_flutter/features/common/platform/platform_funnel/platform_funnel.dart';
+import 'package:url_launcher/url_launcher.dart';
 
+import '../constant/constants.dart';
 import '../data_source/local/local_practice_theme_storage.dart';
 
 class SocialLoginCtr extends GetxController {
@@ -31,13 +33,15 @@ class SocialLoginCtr extends GetxController {
   Rx<SocialLoginChoiceViewState> loginChoiceViewState = Rx<SocialLoginChoiceViewState>(SocialLoginChoiceViewState.waitingToLogin);
   Rx<bool> loginChoiceViewLoginFailed = Rx<bool>(false);
   Rx<SocialLoginBottomSheetState> socialLoginBottomSheetState = Rx<SocialLoginBottomSheetState>(SocialLoginBottomSheetState.choiceView);
-  Rx<String> firstName = Rx<String>('');
-  Rx<String> lastName = Rx<String>('');
-  Rx<DateTime> birthday = Rx<DateTime>(DateTime(2000, 1, 1));
+  Rx<String> firstName = Rx<String>('default');
+  Rx<String> lastName = Rx<String>('default');
+  Rx<DateTime?> birthday = Rx<DateTime?>(null);
   Rx<UserGender> gender = Rx<UserGender>(UserGender.unknown);
   Rx<String> nickname = Rx<String>('');
   Rx<UserAdditionalInfoViewState> userAdditionalInfoViewState = Rx<UserAdditionalInfoViewState>(UserAdditionalInfoViewState.input);
   Rx<bool> userAdditionalInfoViewLoginFailed = Rx<bool>(false);
+
+  RxBool checkPrivacyAgreement = false.obs;
 
   bool checkDefaultThemeId = false;
 
@@ -231,7 +235,7 @@ class SocialLoginCtr extends GetxController {
       UserAdditionalInfoModel userAdditionalInfoModel = UserAdditionalInfoModel(
           firstName: firstName.value,
           lastName: lastName.value,
-          birth: formatDate(birthday.value),
+          birth: formatDate(birthday.value ?? DateTime(1900,0,0)),
           gender: formatGender(gender.value),
           nickName: nickname.value);
       RemoteUserAdditionalInfoDataSource remoteUserAdditionalInfoDataSource = RemoteUserAdditionalInfoDataSource(AuthDioFactory().dio);
@@ -285,5 +289,16 @@ class SocialLoginCtr extends GetxController {
     loginChoiceViewState.value = SocialLoginChoiceViewState.waitingToLogin;
     loginChoiceViewLoginFailed.value = false;
     userAdditionalInfoViewLoginFailed.value = false;
+  }
+
+  void gotoPrivacyPolicy() async {
+
+    Uri privacyPolicyUri = Uri.parse(Constants.privacyPolicyUrl);
+
+    if (await canLaunchUrl(privacyPolicyUri)) {
+      await launchUrl(privacyPolicyUri, mode: LaunchMode.inAppWebView);
+    } else {
+      throw '[gotoPrivacyPolicy] Could not launch $privacyPolicyUri';
+    }
   }
 }
--- lib/features/common/widgets/social_login_choice_view.dart
+++ lib/features/common/widgets/social_login_choice_view.dart
@@ -1,91 +1,136 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 import 'package:swm_peech_flutter/features/common/controllers/social_login_controller.dart';
 import 'package:swm_peech_flutter/features/common/models/social_login_choice_view_state.dart';
 import 'package:swm_peech_flutter/features/common/platform/platform_device_info/platform_device_info.dart';
+import 'package:swm_peech_flutter/features/mypage/controller/mypage_controller.dart';
 
 Widget socialLoginChoiceView(BuildContext context, SocialLoginCtr controller) {
-  return SizedBox(
-    height: 300,
-    child: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          const Text(
-            '3초 간편 로그인 후 계속하기',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
+  return Container(
+    color: Colors.white,
+    child: SizedBox(
+      height: 300,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const Text(
+              '3초 간편 로그인 후 계속하기',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          const SizedBox(height: 20),
-          if (controller.loginChoiceViewState.value == SocialLoginChoiceViewState.waitingToLogin)
-            Column(
-              children: [
-                const Text(
-                  '지금 로그인하면 매달 150분 무료 사용 가능!',
+            const SizedBox(height: 20),
+            if (controller.loginChoiceViewState.value == SocialLoginChoiceViewState.waitingToLogin)
+              Column(
+                children: [
+                  const Text(
+                    '지금 로그인하면 매달 150분 무료 사용 가능!',
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Obx(() =>
+                      Checkbox(value: controller.checkPrivacyAgreement.value, onChanged: (bool? value){
+                        controller.checkPrivacyAgreement.value = value!;
+                      })),
+                      Text("개인정보 처리방침 및 이용약관에 동의합니다.", style: TextStyle(fontSize: 15),),
+                      TextButton(onPressed: (){controller.gotoPrivacyPolicy();}, child: Text("상세보기", style: TextStyle(fontSize: 15, color: Colors.black, fontWeight: FontWeight.normal),), style: ButtonStyle(
+                        fixedSize: MaterialStateProperty.all(Size(80, 40)),  // 고정 크기 설정 (너비: 20, 높이: 30)
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero,  // 모서리 반경을 0으로 설정
+                          ),
+                        ),
+                      ),),
+                    ],
+                  ),
+                  if (controller.loginChoiceViewLoginFailed.value == true)
+                    const Column(
+                      children: [
+                        Text(
+                          '로그인에 실패했습니다. 다시 시도해주세요.',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.red,
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                      ],
+                    )
+                  else
+                    const SizedBox(height: 20),
+                  GestureDetector(
+                    onTap: () async {
+                      if(controller.checkPrivacyAgreement.value == false){
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              backgroundColor: Colors.white,
+                              title: Text("주의"),
+                              content: Text("개인정보 처리에 동의해 주세요."),
+                              actions: [
+                                TextButton(
+                                  child: Text("확인"),
+                                  onPressed: () {
+                                    Navigator.of(context).pop(); // 다이얼로그 닫기
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                        return;
+                      }
+                      controller.loginWithKakao(context);
+                    },
+                    child: Image.asset(
+                      'assets/images/kakao_login_medium_wide.png',
+                    ),
+                  ),
+                  if (PlatformDeviceInfo.isIOS())
+                    Column(
+                      children: [
+                        SizedBox(height: 10),
+                        SizedBox(
+                          width: 300,
+                          child: SignInWithAppleButton(
+                            onPressed: () {
+                              controller.loginWithApple(context);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  // SignInWithAppleButton(
+                  //   onPressed: () async {
+                  //     final AppleAuthProvider provider = AppleAuthProvider();
+                  //     provider.addScope('email');
+                  //     provider.addScope('name');
+                  //     final UserCredential userCredential = await FirebaseAuth.instance.signInWithProvider(provider);
+                  //     print('userCredential: ${userCredential.user?.getIdToken()}');
+                  //   },
+                  // ),
+                ],
+              )
+            else if (controller.loginChoiceViewState.value == SocialLoginChoiceViewState.success)
+              const Text('로그인 성공!',
                   style: TextStyle(
                     fontSize: 15,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                if (controller.loginChoiceViewLoginFailed.value == true)
-                  const Column(
-                    children: [
-                      Text(
-                        '로그인에 실패했습니다. 다시 시도해주세요.',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.red,
-                        ),
-                      ),
-                      SizedBox(height: 5),
-                    ],
-                  )
-                else
-                  const SizedBox(height: 20),
-                GestureDetector(
-                  onTap: () async {
-                    controller.loginWithKakao(context);
-                  },
-                  child: Image.asset(
-                    'assets/images/kakao_login_medium_wide.png',
-                  ),
-                ),
-                if (PlatformDeviceInfo.isIOS())
-                  Column(
-                    children: [
-                      SizedBox(height: 10),
-                      SizedBox(
-                        width: 300,
-                        child: SignInWithAppleButton(
-                          onPressed: () {
-                            controller.loginWithApple(context);
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                // SignInWithAppleButton(
-                //   onPressed: () async {
-                //     final AppleAuthProvider provider = AppleAuthProvider();
-                //     provider.addScope('email');
-                //     provider.addScope('name');
-                //     final UserCredential userCredential = await FirebaseAuth.instance.signInWithProvider(provider);
-                //     print('userCredential: ${userCredential.user?.getIdToken()}');
-                //   },
-                // ),
-              ],
-            )
-          else if (controller.loginChoiceViewState.value == SocialLoginChoiceViewState.success)
-            const Text('로그인 성공!',
-                style: TextStyle(
-                  fontSize: 15,
-                ))
-          else if (controller.loginChoiceViewState.value == SocialLoginChoiceViewState.loading)
-            const CircularProgressIndicator(),
-        ],
+                  ))
+            else if (controller.loginChoiceViewState.value == SocialLoginChoiceViewState.loading)
+              const CircularProgressIndicator(),
+          ],
+        ),
       ),
     ),
   );
